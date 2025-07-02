1ed0d8f946a38d8df28d8fd903d9d25e3073caaa
ldzbeta <ldzbeta25@gmail.com>
2025-07-02 22:18:51 +0530

    Create BFS in adj List.cpp

---
+++ Graphs/BFS in adj List.cpp
@@ -0,0 +1,18 @@
+void bfs(int start, const vector<vector<int>>& adjList, vector<bool>& visited) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";  // Process node (you can modify this as needed)
+
+        for (int neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
